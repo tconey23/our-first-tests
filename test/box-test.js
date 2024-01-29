50ff98c7973541711075c2dc44de2d6f4eaf8dfb
tom coney <tom.ce.coney@gmail.com>
2024-01-29 15:22:17 -0700

    Adds iteration 1

--- test/box-test.js
+++ test/box-test.js
@@ -1,4 +1,9 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-// const {createBox, calculateArea} = require('../box');
+const chai = require('chai');
+const expect = chai.expect;
+const {createBox, calculateArea} = require('../box');
 
+describe('check if true function', function() {
+    it('should always return true', () => {
+        expect(true).to.equal(true);
+    });
+});
